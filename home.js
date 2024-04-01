6b847ddb3e2bcb5ce8602635db84bc8b4c3fe176
AnumolSkaria98 <122593362+AnumolSkaria98@users.noreply.github.com>
2024-04-01 11:43:18 -0700

    Add testimonial section with slides and pagination

--- home.js
+++ home.js
@@ -31,4 +31,29 @@ function toggleNav() {
     });
 });
 
+// Testimonial
+let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        const slides = document.getElementsByClassName("testimonial-slide");
+        const pagination = document.querySelector(".pagination");
+        let dots = '';
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+            dots += `<button class="dot" onclick="currentSlide(${i+1})"></button>`;
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        pagination.innerHTML = dots;
+        pagination.getElementsByTagName('button')[slideIndex - 1].classList.add('active');
+        setTimeout(showSlides, 4000); // Change slide every 4 seconds
+    }
+
+    function currentSlide(n) {
+        slideIndex = n;
+        showSlides();
+    }
+
 
